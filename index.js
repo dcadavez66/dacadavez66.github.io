d995a41730141c83595de0ac8bccc6e37c0661c1
dcadavez66 <diogocadavez@hotmail.com>
2022-10-25 16:01:40 +0200

    new updt

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 
 const mouseMoved = (e) =>{
 
-	var cube = document.getElementById('cube1');
-	var cub2 = document.getElementById('cube2');
-	var cube3 = document.getElementById('cube3');
+	var cubeArray = document.getElementsByClassName("cube");
 	var movX = e.movementX;
 	var movY = e.movementY
 
 	var angleX = 0;
 	var angleY = 0;
 
+	
+
 	if(movX < 0){
 
 		angleX +=180;
-		cube.style.setProperty("--y", '-' +angleX +'deg');
-		cube2.style.setProperty("--y", '-' +angleX +'deg');
-		cube3.style.setProperty("--y", '-' +angleX +'deg');
+		for(let i = 0 ; i < cubeArray.length; i++){
+
+			cubeArray[i].style.setProperty("--y", '-' +angleX +'deg');
+		}
+		
+		
 	}
 	else{
 
-		// angleX +=90;
-		cube.style.setProperty("--y", angleX +'deg');
-		cube2.style.setProperty("--y", angleX +'deg');
-		cube3.style.setProperty("--y", angleX +'deg');
+		angleX +=180;
+		for(let i = 0 ; i < cubeArray.length; i++){
+
+			cubeArray[i].style.setProperty("--y", angleX +'deg');
+		}
 	}
 
 
 	if(movY < 0 ){
 		angleY +=180;
-		cube.style.setProperty("--x", '-' +angleY +'deg');
-		cube2.style.setProperty("--x", '-' +angleY +'deg');
-		cube3.style.setProperty("--x", '-' +angleY +'deg');
+		for(let i = 0 ; i < cubeArray.length; i++){
+
+			cubeArray[i].style.setProperty("--x", '-' +angleY +'deg');
+		}
+		
 	}
 	else{
 
-		// angleY -=90;
-		cube.style.setProperty("--x", '-' +angleY +'deg');
-		cube2.style.setProperty("--x", '-' +angleY +'deg');
-		cube3.style.setProperty("--x", '-' +angleY +'deg');
+		// angleY +=180;
+		for(let i = 0 ; i < cubeArray.length; i++){
 
+			cubeArray[i].style.setProperty("--x", '-' +angleY +'deg');
+		}
 	}
 
 }
